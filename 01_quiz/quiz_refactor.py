928797a51492df20925b90af3d0f5592c254d89c
Jonathan Soukaseume <sksmejn@gmail.com>
2021-11-03 13:33:34 -0700

    chore: create player quit during game

--- 01_quiz/quiz_refactor.py
+++ 01_quiz/quiz_refactor.py
@@ -9,18 +9,29 @@ print("\n\nWelcome to the computer quiz!\n\n")
 playing = input("Would you like to play? (y/n) ").lower()
 if playing != "yes" and playing != "y":
     quit()
+else:
+    playing = True
 
-score = 0
+while playing == True:
+    score = 0
 
-for question in questions:
-    answer = questions[question]
-    userInput = input(f"{question} ").lower()
-    if userInput == answer:
-        print("Correct!\n")
-        score += 1
-    else:
-        print("Incorrect!\n")
+    for question in questions:
+        answer = questions[question]
+        userInput = input(f"{question} ").lower()
+        if userInput == answer:
+            print("Correct!\n")
+            score += 1
+        elif userInput == "quit" or userInput == "q":
+            print("Exiting program........")
+            playing = False
+            break
+        else:
+            print("Incorrect!\n")
+    if playing == False:
+        break
+    print(f"Your score is: {score} out {len(questions)}")
+    print(f"That's {(score/ 4 * 100)} %!")
+    playing = False
 
-print(f"Your score is: {score} out {len(questions)}")
-print(f"That's {(score/ 4 * 100)} %!")
 print("\nThanks for playing!\n")
+quit()
